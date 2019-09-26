95a80c58193cb4c5825c9956fb875e8010fd8bc9
kdclifford <KClifford1@uclan.ac.uk>
2019-09-26 22:39:19 +0100

    enemy cone

---
+++ New Unity Project/Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,29 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+
+//[CustomEditor (typeof (FieldOfView))]
+//public class FieldOfViewEditor : Editor{
+    
+//    void OnSceneGUI()
+//    {
+//        FieldOfView fow = (FieldOfView)target;
+//        Vector3 fovGround = fow.transform.position;
+//        fovGround.y = 0.1f;
+//        Handles.color = Color.white;
+//        Handles.DrawWireArc(fovGround, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+//        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+//        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        
+//        Handles.DrawLine(fovGround, fovGround + viewAngleA * fow.viewRadius);
+//        Handles.DrawLine(fovGround, fovGround + viewAngleB * fow.viewRadius);
+
+//        Handles.color = Color.red;
+//        foreach (Transform visibleTarget in fow.visibleTargets)
+//        {
+//            Handles.DrawLine(fovGround, visibleTarget.position);
+//        }
+//    }
+//}
--- New Unity Project/Assets/PlayerMove.cs
+++ New Unity Project/Assets/PlayerMove.cs
@@ -28,13 +28,13 @@ public class PlayerMove : MonoBehaviour
     }
     private void PlayerMovement()
     {
-        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
-        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+        float horizInput = Input.GetAxis(horizontalInputName);
+        float vertInput = Input.GetAxis(verticalInputName);
 
         Vector3 forwardMovement = transform.forward * vertInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        charController.SimpleMove(forwardMovement + rightMovement);
+        charController.SimpleMove(Vector3.ClampMagnitude( forwardMovement + rightMovement, 1.0f) * movementSpeed);
         JumpInput();
     }
 
