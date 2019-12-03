1c89e8a0e9c2e376aab6e644bcd92dc3b71d4f29
kdclifford <47176557+kdclifford@users.noreply.github.com>
2019-12-03 10:59:02 +0000

    floor

--- New Unity Project/Assets/GameManager.cs
+++ New Unity Project/Assets/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
     private static int x = 5, y = 4, z = 4;
     private List<List<int>> floorList = new List<List<int>>();
 
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
     //private int[, ,] floorList = new int[z, x, y];
     void Awake()
     {
@@ -37,44 +40,54 @@ public class GameManager : MonoBehaviour
 
     void SpawnMap()
     {
-            int i = 0;
-            path = Application.dataPath + "/Map" + i + ".txt";
-        string loadedLine = "";
-        while (!File.Exists(path))
-        {
-            StreamReader sReader = new StreamReader(path);
+        Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.Euler(90.0f, 0.0f, 0.0f));
+    }
 
-            // Create File if path isnt found 
-            if (!File.Exists(path))
-            {
-               // break;
-            }
-            z = i;
-            loadedLine = sReader.ReadLine();
-            x = int.Parse(loadedLine);
-            loadedLine = sReader.ReadLine();
-            y = int.Parse(loadedLine);
 
-            for (int j = 0; j < 7; j++)
-            {
-                floorList.Add(new List<int>());
-                loadedLine = sReader.ReadLine();
-                for (int k = 0; k < y; k++)
-                {                    
-                    //char breakLine = loadedLine[i];
-                    //floorList[k].Add(loadedLine[k] - '0');
-                }
-            }
+//    void SpawnMap()
+//    {
+//            int i = 0;
+//            path = Application.dataPath + "/Map" + i + ".txt";
+//        string loadedLine = "";
+//        while (File.Exists(path))
+//        {
+//            StreamReader sReader = new StreamReader(path);
 
-sReader.Close();
-            i++;
+//            // Create File if path isnt found 
+//            if (!File.Exists(path))
+//            {
+//                //break;
+//            }
+//            z = i;
+//            loadedLine = sReader.ReadLine();
+//            x = int.Parse(loadedLine);
+//            loadedLine = sReader.ReadLine();
+//            y = int.Parse(loadedLine);
+
+//            for (int j = 0; j < x; j++)
+//            {
+//                floorList.Add(new List<int>());
+//                loadedLine = sReader.ReadLine();
+//                for (int k = 0; k < y; k++)
+//                {
+//                    //char breakLine = loadedLine[i];
+//                    //floorList[k].Add(loadedLine[k] - '0');
+
+//                    // Instantiate at position (0, 0, 0) and zero rotation.
+//                    Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+//                }
+//            }
+
+//sReader.Close();
+//            i++;
 
             
-        }
+//        }
         
 
 
-    }
+//    }
 
 
 }
