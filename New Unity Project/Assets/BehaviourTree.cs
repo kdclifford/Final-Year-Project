d4ec97c032625de1724ce5f6ad55a9eb2c16c5ef
kyle <KClifford1@uclan.ac.uk>
2020-01-07 19:16:26 +0000

    start the behaviour tree class

--- New Unity Project/Assets/BehaviourTree.cs
+++ New Unity Project/Assets/BehaviourTree.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class BehaviourTree : MonoBehaviour
 {
-    
+
     public GameObject[,] Map;
     private GameObject gameManager;
     private GameObject playerObject;
@@ -16,18 +16,49 @@ public class BehaviourTree : MonoBehaviour
 
 
     int playerFootSteps;
-    int sizeOfList;
+    int playerList;
     private List<Transform> visibleTargets = new List<Transform>();
+
+
+    bool HearThePlayer()
+    {
+        if (playerFootSteps > 0)
+        {
+            for (int i = 0; i < playerFootSteps; i++)
+            {
+                if (playerTargetList.footStepTargets[i].transform == this.transform)
+                {
+                    // Message with a GameObject name.
+                    Debug.Log("I Hear the Player " + this.gameObject.name);
+
+                    //transform.LookAt(playerObject.transform);
+                    //coneColour.material = alertColour;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool SeeThePlayer()
+    {
+        if (playerList > 0)
+        {
+            Debug.Log("I see the Player " + this.gameObject.name);
+            return true;
+        }
+        return false;
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
         enemyObject = this.gameObject;
-        gameManager = GameObject.FindGameObjectWithTag("Game Manager");
-        Map = gameManager.GetComponent<GameManager>().floorList;
+        //gameManager = GameObject.FindGameObjectWithTag("Game Manager");
+        //Map = gameManager.GetComponent<GameManager>().floorList;
         playerObject = GameObject.FindGameObjectWithTag("Player");
-       // fieldOfViewAI = gameManager.GetComponent<GameManager>()
+        // fieldOfViewAI = gameManager.GetComponent<GameManager>()
     }
 
     // Update is called once per frame
@@ -36,31 +67,14 @@ public class BehaviourTree : MonoBehaviour
         visibleTargets = enemyObject.GetComponent<FieldOfView>().visibleTargets;
         playerTargetList = enemyObject.GetComponent<FieldOfView>().playerTargetList;
 
-        sizeOfList = visibleTargets.Count;
+        playerList = visibleTargets.Count;
         playerFootSteps = playerTargetList.footStepTargets.Count;
 
-        if (sizeOfList > 0)
-            {
-                //oldRotation = transform.rotation;
-                transform.LookAt(visibleTargets[0].transform);
-            }
-            else if (playerFootSteps > 0)
-            {
-                for (int i = 0; i < playerFootSteps; i++)
-                {
-                    if (playerTargetList.footStepTargets[i].transform == this.transform)
-                    {
-                        transform.LookAt(playerObject.transform);
-                        //coneColour.material = alertColour;
+        if (SeeThePlayer() || HearThePlayer())
+        {
+
+        }
 
 
-                    }
-                    else
-                    {
-                        //coneColour.material = normalColour;
-                    }
-                }
-            }
-        
     }
 }
