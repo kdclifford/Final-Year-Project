6d53e57aeb6e3bda4128e9b3c6f3302e47bbcc41
kyle <KClifford1@uclan.ac.uk>
2020-01-11 17:01:08 +0000

    tree

--- New Unity Project/Assets/BehaviourTree.cs
+++ New Unity Project/Assets/BehaviourTree.cs
@@ -3,6 +3,86 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+//Node States
+public enum NodeState
+{
+    Success,
+    Running,
+    Failure,
+}
+
+//Parent Class of All the behaviour tree Nodes
+public abstract class Node
+{
+    public Node() { }
+
+    private NodeState mCurrentNodeState;
+
+    //Getter for the Current Node State
+    public NodeState GetNodeState
+    {
+        get
+        {
+            return mCurrentNodeState;
+        }
+    }
+
+    public NodeState SetNodeState
+    {
+        set
+        {
+            mCurrentNodeState = value;
+        }
+    }
+    public abstract NodeState TreeStatus();
+
+}
+
+//Selector Node Derived from Parent class node
+public class Selector : Node
+{
+    private List<Node> mChildNodes = new List<Node>();
+
+    public Selector(List<Node> childNodes)
+    {
+        mChildNodes = childNodes;
+    }
+
+    public override NodeState TreeStatus()
+    {
+        foreach (Node loopNode in mChildNodes)
+        {
+            if (loopNode.GetNodeState == NodeState.Success)
+            {
+                SetNodeState = NodeState.Success;
+                return GetNodeState;
+            }
+            else if (loopNode.GetNodeState == NodeState.Running)
+            {
+                SetNodeState = NodeState.Running;
+                return GetNodeState;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        SetNodeState = NodeState.Failure;
+        return GetNodeState;        
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
 public class BehaviourTree : MonoBehaviour
 {
@@ -18,7 +98,6 @@ public class BehaviourTree : MonoBehaviour
     int playerFootSteps;
     int playerList;
     private List<Transform> visibleTargets = new List<Transform>();
-
 
     bool HearThePlayer()
     {
