eeadbed8073e805b0cbe029f3d5431adaefb6c03
kyle <KClifford1@uclan.ac.uk>
2020-01-18 17:33:02 +0000

    mini map

---
+++ New Unity Project/Assets/MiniMapMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapMove : MonoBehaviour
+{
+    public Transform player;    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+       
+    }
+}
