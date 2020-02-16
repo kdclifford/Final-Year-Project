f96dacf858b3db55bdce7a15c0bc6a9c62ea8f2b
kdclifford <47176557+kdclifford@users.noreply.github.com>
2020-02-16 22:05:22 +0000

    UI stuff

---
+++ New Unity Project/Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SwitchedCamera;
+    [SerializeField]
+    private GameObject player;
+    private bool IsSwitched = false;
+    private Vector3 oldCameraPos = new Vector3();
+
+    private GameObject UiCanvas;
+    private GameObject SideMenu;
+    private GameObject MiniMap;
+    private GameObject HealthBar;
+    private GameObject GameManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        SwitchedCamera = GameObject.FindGameObjectWithTag("PlayerCamera");
+        oldCameraPos = SwitchedCamera.transform.localPosition;
+        UiCanvas = GameObject.FindGameObjectWithTag("ScreenCanvas");
+        SideMenu = UiCanvas.transform.Find("SideMenu").gameObject;
+        MiniMap = UiCanvas.transform.Find("MiniMapHolder").gameObject;
+        HealthBar = UiCanvas.transform.Find("HealthBarHolder").gameObject;
+        GameManager = GameObject.FindGameObjectWithTag("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchedCamera.GetComponent<PlayerCamera>().enabled = IsSwitched;
+            player.GetComponent<PlayerMove>().enabled = IsSwitched;
+            SwitchedCamera.GetComponent<CameraMove>().enabled = !IsSwitched;
+                GameManager.GetComponent<MouseClick>().enabled = !IsSwitched;
+
+            // if cam free roam
+            if (!IsSwitched)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                SwitchedCamera.transform.parent = null;
+                SideMenu.SetActive(true);
+                MiniMap.SetActive(false);
+                HealthBar.SetActive(false);
+            }
+            else // locked camera
+            {
+                SideMenu.SetActive(false);
+                MiniMap.SetActive(true);
+                HealthBar.SetActive(true);
+                Cursor.lockState = CursorLockMode.Locked;
+                SwitchedCamera.transform.parent = player.transform;
+                SwitchedCamera.transform.localPosition = oldCameraPos;
+                SwitchedCamera.transform.rotation = SwitchedCamera.transform.parent.rotation;          
+            }
+
+            IsSwitched = !IsSwitched;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    public List<string> ClickTagList;
+
+    private Camera MainCam;
+
+
+    private void Start()
+    {
+        MainCam = GameObject.FindWithTag("PlayerCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(MainCam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+
+
+
+
+            if (results.Count > 0)
+            {
+                //WorldUI is my layer name
+                if (results[0].gameObject.layer == LayerMask.NameToLayer("UI"))
+                {
+                    Debug.Log("Hit UI");
+                    string dbg = "Root Element: {0} \n GrandChild Element: {1}";
+                    //Debug.Log(string.Format(dbg, results[results.Count - 1].gameObject.name, results[0].gameObject.name));
+                    results.Clear();
+                }
+            }
+            else
+            {
+                if (hit)
+                {
+                //Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+
+                    Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+                    foreach (var s in ClickTagList)
+                    {
+                        if (hitInfo.transform.gameObject.tag == s)
+                        {
+                           // CurrentSelected = hitInfo.transform.gameObject;
+
+                            Debug.Log("It's Working!");
+                            break;
+                        }
+
+                    }
+
+                }
+            }
+
+
+            Debug.Log("Mouse is down");
+        }
+
+     
+    }
+
+}
