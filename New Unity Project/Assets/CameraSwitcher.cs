b9d79ab65c6d5f722a73454287457aa17127e99c
kdclifford <47176557+kdclifford@users.noreply.github.com>
2020-02-16 19:29:37 +0000

    camera stuff

---
+++ New Unity Project/Assets/CameraSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SwitchedCamera;
+    [SerializeField]
+    private GameObject player;
+    private bool IsSwitched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        SwitchedCamera = GameObject.FindGameObjectWithTag("PlayerCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchedCamera.GetComponent<PlayerCamera>().enabled = IsSwitched;
+            player.GetComponent<PlayerMove>().enabled = IsSwitched;
+            SwitchedCamera.GetComponent<CameraMove>().enabled = !IsSwitched;
+
+            if (!IsSwitched)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+                SwitchedCamera.transform.parent = null;
+
+            }
+
+            IsSwitched = !IsSwitched;
+        }
+    }
+}
---
+++ New Unity Project/Assets/MenuInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuInfo : MonoBehaviour
+{
+
+    public Text NameText;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
